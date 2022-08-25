96334c75b153b1cdb87945b18b0bc4921b83eb01
Yuvraaj Narula <ynarula678@gmail.com>
2022-08-26 00:13:40 +0530

    optimizejs

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+
 const algoget = ()=>{
     fetch("https://xino-creative.herokuapp.com/").then(async (data) => {
         data = await data.json();
         console.log(data)
         let [x,y,z] = data
         var i = `
-        Your optimal electricty usage is Mains to Lights - ${x} units 
-        Mains to Thermal Regulators - ${y} units 
-        Mains to Cooking Appliances - ${z} units 
-        Solar to Lights = (${10-x}) 
-        Solar to Thermal = (${20-y}) 
-        Solar to Cooking = (${15-z})
+        Your optimal electricty usage is Mains to Lights - ${x} units <br>
+        Mains to Thermal Regulators - ${y} units <br>
+        Mains to Cooking Appliances - ${z} units <br>
+        Solar to Lights = (${10-x}) <br>
+        Solar to Thermal = (${20-y}) <br>
+        Solar to Cooking = (${15-z})<br>
         `
-        document.querySelector('.opitmize-info').innerText = i
+        document.querySelector('.optimize-info').innerHTML = i;
     });
 };
