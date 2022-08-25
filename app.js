6dd271d959b6f3fbead107ad3e3a8de6763f32a9
Yuvraaj Narula <ynarula678@gmail.com>
2022-08-26 00:03:14 +0530

    optimizejs

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-document.querySelector('#submit').addEventListener('click',()=>{
-    fetch('https://xino-creative.herokuapp.com/')
-    .then((data) => {
-        console.log(data);
+const algoget = ()=>{
+    fetch("https://xino-creative.herokuapp.com/").then(async (data) => {
+        data = await data.json();
+        console.log(data)
+        let [x,y,z] = data
+        var i = `
+        Your optimal electricty usage is Mains to Lights - ${x} units 
+        Mains to Thermal Regulators - ${y} units 
+        Mains to Cooking Appliances - ${z} units 
+        Solar to Lights = (${10-x}) 
+        Solar to Thermal = (${20-y}) 
+        Solar to Cooking = (${15-z})
+        `
+        document.querySelector('.opitmize-info').innerText = i
     });
-})
+};
